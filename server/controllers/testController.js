0207385d212a1771b55fe53880f208d421fb6318
kunalg3 <kunalgoyal446@gmail.com>
2024-09-15 23:21:21 +0530

    server built and test route setup

---
+++ server/controllers/testController.js
@@ -0,0 +1,4 @@
+const test=async(req,res)=>{
+    res.json({message:'I am test'})
+}
+module.exports=test;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-xL5aWuV6l9gSjA6N
+const express=require('express')
+const cors=require('cors')
+const testRoutes =require('./routes/testRoutes')
+
+const app=express()
+
+const PORT=process.env.PORT||8000
+
+app.use(express.json())
+app.use(cors({
+    origin:[],
+    credentials:true
+}))
+
+app.use('/test',testRoutes)
+
+require('./config/dbConfig')
+
+app.listen(PORT,()=>{console.log(`App listening on port ${PORT}`)})
